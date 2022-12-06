0d61f60c545c38d220a0b3c87534f45cb8c58c1e
Hector Romero <hectorromerodev@gmail.com>
2022-12-05 17:33:51 -0800

    add provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import Home from './pages/Home';
 
-const App: React.FC = () => <Home />;
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+};
 
 export default App;
