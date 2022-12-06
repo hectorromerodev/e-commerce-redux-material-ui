fd13416015d44dd15f32b7eaf8f08b97db923617
Hector Romero <hectorromerodev@gmail.com>
2022-12-05 17:34:16 -0800

    add initial models

---
+++ src/models/Product.interface.ts
@@ -0,0 +1,7 @@
+export default interface Product {
+  id: number | string;
+  title: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
---
+++ src/models/ProductState.interface.ts
@@ -0,0 +1,7 @@
+import Product from './Product.interface';
+
+export default interface ProductState {
+  products: Product[];
+  isLoading: boolean;
+  error: string | null;
+}
