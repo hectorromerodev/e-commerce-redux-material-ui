45ce7db357518380fbb431f4908043155c0c72de
Hector Romero <hectorromerodev@gmail.com>
2022-12-05 20:25:16 -0800

    adding redux-persist and config

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,43 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import authReducer from './slices/auth';
 import productsReducer from './slices/products';
 
+const persistAutConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token']
+};
+
+const persistProductsConfig = {
+  key: 'products',
+  storage,
+  whitelist: ['products']
+};
+
 const store = configureStore({
   reducer: {
     // Add your reducers here
-    auth: authReducer,
-    products: productsReducer
-  }
+    auth: persistReducer<ReturnType<typeof authReducer>>(
+      persistAutConfig,
+      authReducer
+    ),
+    products: persistReducer<ReturnType<typeof productsReducer>>(
+      persistProductsConfig,
+      productsReducer
+    )
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type Dispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
 
 export default store;
