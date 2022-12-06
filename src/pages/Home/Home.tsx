e618d1cb7c9aaa7595583d673543c00ec7809a4e
Hector Romero <hectorromerodev@gmail.com>
2022-12-05 19:57:52 -0800

    useDispatch on home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-import { useCustomSelector } from '@/hooks/redux';
+import { useCustomSelector, useCustomDispatch } from '@/hooks/redux';
+import { setToken } from '@/redux/slices/auth';
 
 const Home: React.FC = () => {
   const { auth } = useCustomSelector((state) => state);
+  const dispatch = useCustomDispatch();
 
-  console.log(auth);
+  const handleLogin = (): void => {
+    dispatch(setToken('Token-klaljsd78-8duas9das8'));
+  };
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home page works!</h1>
+      <button onClick={handleLogin}>Login</button>
+      <p>{auth.token}</p>
+    </div>
+  );
 };
 
 export default Home;
